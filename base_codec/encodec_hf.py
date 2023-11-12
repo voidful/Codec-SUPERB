40fe719961ce3e3b6741920f5f082cefc6a71018
voidful <voidful.stack@gmail.com>
2023-11-12 19:31:15 +0800

    refactoring

---
+++ base_codec/encodec_hf.py
@@ -0,0 +1,30 @@
+from transformers import AutoModel, AutoProcessor
+from codec.general import save_audio
+
+
+class BaseCodec:
+    def __init__(self):
+        self.config()
+        self.model = AutoModel.from_pretrained(self.pretrained_model_name)
+        self.processor = AutoProcessor.from_pretrained(self.pretrained_model_name)
+        self.sampling_rate = self.processor.sampling_rate
+
+    def config(self):
+        self.pretrained_model_name = "facebook/encodec_24khz"
+
+    def synth(self, data):
+        encoder_outputs, padding_mask = self.extract_unit(data, directly_unit=False)
+        audio_values = \
+            self.model.decode(encoder_outputs.audio_codes, encoder_outputs.audio_scales, padding_mask)[0]
+        audio_path = f"dummy_{self.pretrained_model_name}/{data['id']}.wav"
+        save_audio(audio_values[0], audio_path, self.sampling_rate)
+        data['audio'] = audio_path
+        return data
+
+    def extract_unit(self, data, directly_unit=False):
+        audio_sample = data["audio"]["array"]
+        inputs = self.processor(raw_audio=audio_sample, sampling_rate=self.sampling_rate, return_tensors="pt")
+        encoder_outputs = self.model.encode(inputs["input_values"], inputs["padding_mask"])
+        if directly_unit:
+            return encoder_outputs.audio_codes
+        return encoder_outputs, inputs["padding_mask"]
