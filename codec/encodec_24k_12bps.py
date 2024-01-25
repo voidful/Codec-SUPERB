98a5fb24acb72178132d36aee6c1a37a553ce327
hbwu-ntu <f07921092@ntu.edu.tw>
2024-01-25 16:51:11 +0800

    Enable different bps for Encodec

--- codec/encodec_24k_12bps.py
+++ codec/encodec_24k_12bps.py
@@ -4,5 +4,5 @@ class Codec(BaseCodec):
     def config(self):
         self.model = self.EncodecModel.encodec_model_24khz()
         self.model.set_target_bandwidth(12.0)
-        self.setting = "encodec_24khz_12.0"
+        self.setting = "encodec_24khz_12"
         self.sampling_rate = 24_000
