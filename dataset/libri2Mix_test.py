a9024e0981a15e53ebaf1b8c9b1de76ef27aa8e0
Eric Lam <voidful.stack@gmail.com>
2024-01-28 06:21:27 +0800

    fix(dataset): update dataset loading in libri2Mix_test.py

--- dataset/libri2Mix_test.py
+++ dataset/libri2Mix_test.py
@@ -2,7 +2,7 @@ from datasets import load_dataset
 
 
 def load_data():
-    dataset = load_dataset("DynamicSuperb/SourceSeparation_libri2Mix_test")
+    dataset = load_dataset("DynamicSuperb/SourceSeparation_libri2Mix_test")['test']
 
     def map_file_to_id(data):
         data['id'] = "".join(data['file'].split("/")[-2:])
@@ -10,4 +10,5 @@ def load_data():
 
     dataset = dataset.map(map_file_to_id)
     cleaned_dataset = dataset.remove_columns(['file', 's1', 's2', 'instruction'])
+
     return cleaned_dataset
