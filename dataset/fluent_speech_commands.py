8d1ac966757c1f0b31658ee31599c3c4a2c2a1e4
voidful <voidful.stack@gmail.com>
2024-01-28 18:26:59 +0800

    feat(dataset): add function to load and clean Fluent Speech Commands dataset

---
+++ dataset/fluent_speech_commands.py
@@ -0,0 +1,8 @@
+from datasets import load_dataset
+
+
+def load_data():
+    cleaned_dataset = load_dataset("DynamicSuperb/IntentClassification_FluentSpeechCommands", "test")
+    cleaned_dataset = cleaned_dataset.remove_columns(
+        ['file', 'speaker_id', 'transcription', 'action', 'object', 'location', 'instruction'])
+    return cleaned_dataset
