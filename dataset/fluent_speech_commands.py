d0c0d7fdd0b1e9741b5d7f16675df4484b11030f
voidful <voidful.stack@gmail.com>
2024-01-28 21:49:24 +0800

    fix(dataset): update column name in remove_columns function

--- dataset/fluent_speech_commands.py
+++ dataset/fluent_speech_commands.py
@@ -4,5 +4,5 @@ from datasets import load_dataset
 def load_data():
     cleaned_dataset = load_dataset("DynamicSuperb/IntentClassification_FluentSpeechCommands", "test")
     cleaned_dataset = cleaned_dataset.remove_columns(
-        ['file', 'speaker_id', 'transcription', 'action', 'object', 'location', 'instruction'])
+        ['file', 'speakerId', 'transcription', 'action', 'object', 'location', 'instruction'])
     return cleaned_dataset
