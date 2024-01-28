33f4f42e0fb7ea344be880264e8533daff7da9fb
voidful <voidful.stack@gmail.com>
2024-01-28 21:53:14 +0800

    fix(dataset): select 'test' subset from cleaned_dataset in load_data function

--- dataset/fluent_speech_commands.py
+++ dataset/fluent_speech_commands.py
@@ -5,4 +5,5 @@ def load_data():
     cleaned_dataset = load_dataset("DynamicSuperb/IntentClassification_FluentSpeechCommands", "test")
     cleaned_dataset = cleaned_dataset.remove_columns(
         ['file', 'speakerId', 'transcription', 'action', 'object', 'location', 'instruction'])
+    cleaned_dataset = cleaned_dataset['test']
     return cleaned_dataset
