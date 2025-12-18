465f8a97f300441c6b066d55fe33b31a61dc0564
Eric Lam <voidful.stack@gmail.com>
2025-12-19 03:06:23 +0800

    feat: add 1d codec support, update setup dependencies, and cleanup metrics script

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='SoundCodec',
     version='1.7',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=requirements,
+    extras_require={
+        'all': [
+            'funasr',
+            's3tokenizer',
+            'bigcodec',
+            'encodec',
+            'descript-audio-codec',
+            'funcodec @ git+https://github.com/voidful/FunCodec.git',
+            'AudioDec @ git+https://github.com/voidful/AudioDec.git',
+            'wavtokenizer @ git+https://github.com/voidful/WavTokenizer.git'
+        ]
+    }
 )
